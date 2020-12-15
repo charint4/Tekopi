1875036d629babe467bef38063a6f639e4f79156
Neutralix <58196205+neutralix@users.noreply.github.com>
2020-12-15 17:56:51 +0700

    build database

---
+++ database/migrations/2020_12_15_082924_cart_pelanggan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CartPelanggan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_pelanggan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idUser');
+            $table->foreign('idUser')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('id_prod');
+            $table->foreign('id_prod')->references('id_prod')->on('produk')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_pelanggan');
+    }
+}
